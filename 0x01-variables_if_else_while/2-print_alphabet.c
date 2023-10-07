b398c2a476f9e062868d68e536ccd267bb2d5690
lensonlovescode <lensonisinparis@gmail.com>
2023-10-07 10:43:38 -0700

    jkl

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,14 +8,12 @@
  */
 int main(void)
 {
-	char letters[26] = "abcdefghijklmnopqrstuvwxyz";
+	char letters[26] = "abcdefghijklmnopqrstuvwxyz\n";
 	int i;
 
 	for (i = 0; i <= 26; i++)
 	{
 		putchar(letters[i]);
-		putchar('\n');
 	}
-
 	return (0);
 }
