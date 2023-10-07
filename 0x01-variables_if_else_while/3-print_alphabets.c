b76325647fbaff60fabc52a81c4cabf9ad3deb08
lensonlovescode <lensonisinparis@gmail.com>
2023-10-07 11:10:23 -0700

    upper case upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char letters[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char letters[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0; i < 26; i++)
