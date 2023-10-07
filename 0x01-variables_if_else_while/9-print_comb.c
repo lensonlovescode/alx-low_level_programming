03c9f066eea03aae405318b224f5c3b3f69104ae
lensonlovescode <lensonisinparis@gmail.com>
2023-10-07 13:25:50 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: this returns 0
+ *
+ * putchar - prints the numbers to the console
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar('\n');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
