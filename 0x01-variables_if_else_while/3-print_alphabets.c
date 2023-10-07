808fed401be870dec38f6fe5dcd6d22940474b8a
lensonlovescode <lensonisinparis@gmail.com>
2023-10-07 11:06:50 -0700

    upper case alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: this returns 0
+ *
+ * putchar - prints the alphabets to the console
+ */
+int main(void)
+{
+	char letters[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(letters[i]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
