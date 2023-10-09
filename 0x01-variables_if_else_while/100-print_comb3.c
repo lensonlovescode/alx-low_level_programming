6022381503455e0839848f612bf81d74c2f83778
lensonlovescode <lensonisinparis@gmail.com>
2023-10-09 09:45:46 -0700

    tkl

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is just the main function
+ *
+ * Return: this returns 0
+ *
+ * putchar - this prints out all the number combinations
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+		{
+			putchar(i);
+		}
+}
