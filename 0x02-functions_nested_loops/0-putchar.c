4cae1e628d882ed471d594326294e119563634e9
lensonlovescode <lensonisinparis@gmail.com>
2023-10-11 03:21:47 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: this will return 0
+ *
+ */
+int main(void)
+{
+	printf("_putchar");
+	printf("\n");
+
+	return (0);
+}
