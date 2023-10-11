a01ff7f76dd6e6d685a009f0c438ce09c04c31a2
lensonlovescode <lensonisinparis@gmail.com>
2023-10-11 13:15:27 -0700

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * jack_bauer - this will iterate the minuites and hours
+ *
+ * Return: this will return 0
+ *
+ */
+void jack_bauer(void)
+{
+	int H;
+	int h;
+	int M;
+	int m;
+
+	for (H = 0; H <= 2; H++)
+	{
+	for (h = 0; h <= 4; h++)
+	{
+	for (M = 0; M <= 5; M++)
+	{
+	for (m = 0; m <= 9; m++)
+	{
+		_putchar(H + '0');
+		_putchar(h + '0');
+		_putchar(':');
+		_putchar(M + '0');
+		_putchar(m + '0');
+		_putchar('\n');
+	}
+	}
+	}
+	}
+}
