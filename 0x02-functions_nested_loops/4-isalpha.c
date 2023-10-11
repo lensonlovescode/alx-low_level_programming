b159bb661696521c01af9b231f254d49e2f20c1b
lensonlovescode <lensonisinparis@gmail.com>
2023-10-11 11:32:00 -0700

    4 is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,14 +10,8 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
+	if ((c >= 'a' && c >= 'z') || (c >= 'Z' && c >= 'Z'))
 		return (1);
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	}
 	else
 		return (0);
 }
