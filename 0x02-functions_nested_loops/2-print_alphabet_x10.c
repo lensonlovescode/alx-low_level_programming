7130d6604d4fc352715e6abacea87ffd7a66dfd1
lensonlovescode <lensonisinparis@gmail.com>
2023-10-11 10:23:33 -0700

    10 rows of alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,13 @@
  */
 void print_alphabet_x10(void)
 {
-	char i;
 	int j;
+	char i;
 
-	for (j = 10; j < 10; j++)
+	for (j = 0; j < 10; j++)
 	{
 		for (i = 'a' ; i <= 'z' ; i++)
-	{
-		_putchar(i);
+			_putchar(i);
 		_putchar('\n');
-	}
 	}
 }
