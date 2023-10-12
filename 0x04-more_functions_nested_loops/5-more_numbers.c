2d3e822b31f6a1c0c87a0b0c2033d627134cd016
lensonlovescode <lensonisinparis@gmail.com>
2023-10-12 14:10:50 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,16 +12,12 @@ void more_numbers(void)
 
 	for (j = 0; j < 10; j++)
 	{
-	for (i = 0; i <= 14; i++)
-	{
-		if (i > 9)
+		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i / 10 + '0');
+			if (i > 9)
+				_putchar(i / 10 + '0');
 			_putchar(i % 10 + '0');
 		}
-		else
-			_putchar(i + '0');
-	}
 		_putchar('\n');
 	}
 
