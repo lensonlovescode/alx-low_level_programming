6851d2e04a77fd94c10d8a2e57bd5fa84d32a064
lensonlovescode <lensonisinparis@gmail.com>
2023-10-12 12:11:25 -0700

    is it a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - this checks whether c is a digit
+ * @c: this is the value to be evalclient_loop
+ * Return: this will either be 0 or 1
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
