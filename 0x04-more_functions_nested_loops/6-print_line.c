374b5e083e2627c581a070f657c1816b69f06161
lensonlovescode <lensonisinparis@gmail.com>
2023-10-14 08:10:34 -0700

    lineslineslines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - this will print the line
+ * @n: this is the number of times the line should be printed
+ * Return: this will return 0
+ */
+void print_line(int n)
+{
+	int j;
+	
+	for (j = 0; j < n; j++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+
+}
