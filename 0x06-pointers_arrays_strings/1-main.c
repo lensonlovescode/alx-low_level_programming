2fe43d30f76e4618c1edac91cdfb62c84b12cc5c
lensonlovescode <lensonisinparis@gmail.com>
2023-10-18 10:32:39 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-main.c
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-    char s1[98] = "Hello ";
+    char s1[1100] = "Hello ";
     char s2[] = "World!\n";
     char *ptr;
 
@@ -18,7 +18,7 @@ int main(void)
     printf("%s\n", s1);
     printf("%s", s2);
     printf("%s\n", ptr);
-    ptr = _strncat(s1, s2, 7);
+    ptr = _strncat(s1, s2, 1024);
     printf("%s", s1);
     printf("%s", s2);
     printf("%s", ptr);
