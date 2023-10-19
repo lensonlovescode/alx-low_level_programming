b8283b50d2882d43660ab87c726bc30670e92899
lensonlovescode <lensonisinparis@gmail.com>
2023-10-19 14:19:28 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,10 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int flag;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (s1[i] == s2[i])
-		{
-			flag = 0;
-		}
-		else
-		{
-			flag = s1[i] - s2[i];
-		}
+		break;
 	}
-	return (flag);
+	return (s1[i] - s2[i]);
 }
