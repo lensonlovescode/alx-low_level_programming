1d34cfff07f1953778dc1d4a6b4b7867c935716a
lensonlovescode <lensonisinparis@gmail.com>
2023-10-19 12:21:37 -0700

    _strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,17 +11,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *p = dest;
-	char *q = src;
 	int i;
 
-	for (i = 0; i < n && q[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		p[i] = q[i];
+		dest[i] = src[i];
 	}
 	for ( ; i < n; i++)
 	{
-		p[i] = '\0';
+		dest[i] = '\0';
 	}
-	return (p);
+	return (dest);
 }
