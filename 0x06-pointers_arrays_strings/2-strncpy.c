6d7cc1b4fc14013a249300894d6c9e382f46735f
lensonlovescode <lensonisinparis@gmail.com>
2023-10-19 11:20:43 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 #include <string.h>
-/**
- *
- *
+/**	
+ * _strncpy - this copies the source string to the destination
+ * @dest: this is the destination string
+ * @src: this is the source string
+ * @n: this is the number of characters to be copied
+ * Return: this will return the destination string
  *
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	
+	char *p = dest;
+	char *q = src;
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		p[i] = q[i];
+		p[sizeof(p)] = '\0';
+	}
+	return (p);
 }
