c03267556cd0e632d32e38f6cc0771ef887d9e96
lensonlovescode <lensonisinparis@gmail.com>
2023-10-20 22:49:50 +0300

    char

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,13 +9,11 @@ void reverse_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n/2; i++)
 	{
-		for (k = n; k > n; k--)
-		{
-			a[i] = a[k]
-		}
-		return (a);
+		ch = a[i]
+		a[i] = a[n-1-i]
 	}
+}
 	return (a);
 }
