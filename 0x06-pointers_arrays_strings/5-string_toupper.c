1ce6a2e81717ee33d3bb1459ce798966412c338b
lensonlovescode <lensonisinparis@gmail.com>
2023-10-20 13:48:44 -0700

    numb

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
-* *string_toupper - this changes all lowercase letters of a string to uppercase
-* @strn: this is the sting to be reversed
-* return: this will return the uppercase letter's string
+* string_toupper - this changes all lowercase letters of a string to uppercase
+* @strn: this is the sting to be converted to uppercase
+* Return: this will return the string with uppercase only
 */
 char *string_toupper(char *strn)
 {
-    int i;
+	int i;
 
-    for (i = 0; i != '\0'; i++)
-    {
-        if ('a' <= strn[i] && strn[i] <= 'z')
-        {
-            strn[i] = strn[i] - 32;
-        }
-    }
-    return (strn);
+	for (i = 0; i != '\0'; i++)
+	{
+	if ('a' <= strn[i] && strn[i] <= 'z')
+	{
+		strn[i] = strn[i] - 32;
+	}
+	}
+	return (strn);
 }
