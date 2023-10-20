f7cdd0ad08cfbdd03b06600e99b9ffd21bc117a7
lensonlovescode <lensonisinparis@gmail.com>
2023-10-20 14:46:37 -0700

    to be continued

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,28 @@
 #include "main.h"
 /**
-* string_toupper - this changes all first letters of a word to uppercase
+* cap_string - this changes all first letters of a word to uppercase
 * @strn: this is the sting to be converted to uppercase
-* Return: this will return the string with the first letter of words as uppercase
+* Return: it will return the string with the first letter of words uppercase
 */
 char *cap_string(char *strn)
 {
 	int i;
-    int j;
+	int j;
 
-    char special_chars[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	char special_chars[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
 	for (i = 0; strn[i] != '\0'; i++)
 	{
-        for (j = 0; j < 10; j++)
-        {
-	        if (strn[i] == special_chars[j])
-	        {
-                if ('a' <= strn[i + 1] && strn[i + 1] <= 'z')
-                {
-		            strn[i + 1] = strn[i + 1] - 32;
-                }
-	        }
-        }
+	for (j = 0; j < 10; j++)
+	{
+		if (strn[i] == special_chars[j])
+		{
+			if ('a' <= strn[i + 1] && strn[i + 1] <= 'z')
+			{
+			strn[i + 1] = strn[i + 1] - 32;
+			}
+		}
+	}
 	}
 	return (strn);
 }
