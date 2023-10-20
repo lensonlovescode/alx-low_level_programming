2d3c69dc3cf6f48e2e4c3546e0aeaaa1fd62edb0
lensonlovescode <lensonisinparis@gmail.com>
2023-10-20 23:17:17 +0300

    function created

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* *string_toupper - this changes all lowercase letters of a string to uppercase
+* @*: this is the sting to be reversed
+* return: this will return the uppercase letter's string
+*/
+char *string_toupper(char *)
+{
+    int i;
+    char ch;
+
+    for (i = 0; i < n; i++)
+    {
+        if ('a' > *[i] < 'z')
+        {
+            ch = *[i] - 32;
+        }
+    }
+    return (ch);
+}
