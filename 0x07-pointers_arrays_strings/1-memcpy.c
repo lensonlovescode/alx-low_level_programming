c62998a8dbea160a7b6e6fefdda8853985028f13
lensonlovescode <lensonisinparis@gmail.com>
2023-10-23 09:42:11 -0700

    memcpy and memset

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,18 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _memcpy - this copies the memory area
+ * @dest: this is the destination memory
+ * @src: this is the source memory
+ * @n: this it the nmber of bytes to be copied into
+ * Return: this will return a pointer to the destination
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
