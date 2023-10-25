1a06230b2f8b44d9b2a6f83704e8ed13484de996
lensonlovescode <lensonisinparis@gmail.com>
2023-10-25 14:02:01 -0700

    tbc

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *  _puts_recursion - this prints the string followed by a new line
+ *  @s: this is the string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	if (s != '\0';)
+
+		return (s);
+}
