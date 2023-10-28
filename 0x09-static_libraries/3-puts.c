d37479e77a55d4200f40084ac83d15265465f1ff
lensonlovescode <lensonisinparis@gmail.com>
2023-10-28 12:59:09 +0300

    staticc lib

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,5 +1,4 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
  * _puts - prints string to stdout
  * @str: pointer to string to be printed
@@ -10,8 +9,8 @@ void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		_putchar(*str);
+		putchar(*str);
 		str++;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
