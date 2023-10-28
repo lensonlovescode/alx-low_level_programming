ee221f4f51278ffd2ad4542517c63e31a1330834
lensonlovescode <lensonisinparis@gmail.com>
2023-10-28 11:30:23 +0300

    staticcc

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,4 +1,7 @@
-int main()
+#include "main.h"
+
+int main(void)
 {
-	return (0);
+	    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	        return (0);
 }
