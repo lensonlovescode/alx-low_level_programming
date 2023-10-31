867a55a75008b9038ed173995db088ee97a0b7b4
lensonlovescode <lensonisinparis@gmail.com>
2023-10-31 22:28:11 +0300

    arrays

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -26,7 +26,11 @@ char *create_array(unsigned int size, char c)
 		}
 	}
 		return (ptr);
-	if (ptr == NULL || size == 0)
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	if (size == 0)
 	{
 		return (NULL);
 	}
