c8b85078a30295c45ac0287f985afa35daa064f1
lensonlovescode <lensonisinparis@gmail.com>
2023-11-04 13:21:36 +0300

    haaa

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -51,11 +51,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-		for (i = 0; i < len1; i++)
+		for (i = 0; s1[i] != '\0'; i++)
 		{
 			ptr[i] = s1[i];
 		}
-		for (j = 0; j < n; j++)
+		for (j = 0; s2[j] != '\0'; j++)
 		{
 			ptr[len1 + j] = s2[j];
 		}
