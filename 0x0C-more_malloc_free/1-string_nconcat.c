ca96e3c5878283c16c2917b7c66a26ec860609e9
lensonlovescode <lensonisinparis@gmail.com>
2023-11-04 11:12:54 +0300

    best school

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - this concatenates two strings but the second string
+ * only has the first n characters included in the final string.
+ * @n: this is the first number of characters to be included in the
+ * final string
+ * @s1: this is the first string and all it's characters
+ * are to be included
+ * @s2: this is the second string and only the first n characters are to be
+ * included in the string concatenate
+ * Return: this will return a pointer to the concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i;
+	unsigned int j;
+	int len1 = strlen(s1);
+	int len2 = strlen(s2);
+	char *ptr;
+
+	ptr = malloc((len1 + len2) * sizeof(char) + 1);
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	if (s1 != NULL && s2 != NULL)
+	{
+		for (i = 0; i < len1; i++)
+		{
+			ptr[i] = s1[i];
+		}
+		for (j = 0; j < n; j++)
+		{
+			ptr[len1 + j] = s2[j];
+		}
+			ptr[len1 + j + 1] = '\0';
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (ptr);
+}
