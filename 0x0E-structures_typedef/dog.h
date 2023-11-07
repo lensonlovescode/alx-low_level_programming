3ccc6397da15a1dfc805be8f3a71535e40b0f185
lensonlovescode <lensonisinparis@gmail.com>
2023-11-07 13:15:48 +0300

    doja cat

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,19 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
+
 #include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct dog - this is the struct defining the properties of dog
+ * @name: this is the dog's name
+ * @age: this is the dog's age
+ * @owner: this is the dog's owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
