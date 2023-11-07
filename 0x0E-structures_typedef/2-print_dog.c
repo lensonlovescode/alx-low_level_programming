6feb22dee3ffebe92b31a0a248588bf06f11b7b1
lensonlovescode <lensonisinparis@gmail.com>
2023-11-07 19:42:57 +0300

    sensational

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,15 +8,15 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if ((*d).name != NULL)
 		printf("Name: %s\n", (*d).name);
 	else
 		printf("(nil)");
-	if (d != NULL)
+	if ((*d).age >= 0)
 		printf("Age: %f\n", (*d).age);
 	else
 		printf("(nil)");
-	if (d != NULL)
+	if ((*d).owner != NULL)
 		printf("Owner: %s\n", (*d).owner);
 	else
 		printf("(nil)");
