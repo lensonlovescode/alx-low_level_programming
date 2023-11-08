e01c7c7a15ef930efe4ed3f09a63bfba134aef95
lensonlovescode <lensonisinparis@gmail.com>
2023-11-08 23:20:45 +0300

    ~maps

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - this searches for an integer
+ * @array: this is the array tobe searched
+ * @size: this is the size of the array
+ * @cmp: this is the external function to checkthe integer
+ * Return: it will return a pointer to the index
+ * where the return value is not zero
+ *
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) == 1)
+			return (i);
+	}
+	return (-1);
+}
