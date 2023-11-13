cb4a68f6be18af4dd4ca3b291cb65cf74a8e9df9
lensonlovescode <lensonisinparis@gmail.com>
2023-11-13 13:54:33 +0300

    numb

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,45 +1,44 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * string_nconcat - this concatenates two strings
- * @n: the first number of characters to be included in the final string
- * @s1: this is the first string and all it's characters are to be included
- * @s2: the second string and only the first n characters are to be included
- * Return: this will return a pointer to the concatenated string
+ * string_nconcat - Concatenates two strings using at
+ *                  most an inputted number of bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the concatenated space in memory.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i;
-	int len1 = 0;
-	unsigned int len2 = 0;
-	char *ptr;
-	unsigned int j;
+	char *concat;
+	unsigned int len = n, index;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s1[i] != '\0'; i++)
-		len1++;
-	for (i = 0; s2[i] != '\0'; i++)
-		len2++;
-	ptr = malloc(len1 + n + 1);
-	if (ptr == NULL)
+
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
 		return (NULL);
-	if (n >= len2)
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-			ptr[i] = s1[i];
-		for (j = 0; s2[j] != '\0'; j++)
-			ptr[len1 + j] = s2[j];
-		ptr[len1 + j + 1] = '\0';
-	}
-	else
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-			ptr[i] = s1[i];
-		for (j = 0; j < n; j++)
-			ptr[len1 + j] = s2[j];
-	}
-		ptr[len1 + j + 1] = '\0';
-	return (ptr);
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
 }
