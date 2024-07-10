e30df77a0986848903f5095b225dd8757422e0da
lensonlovescode <lensonisinparis@gmail.com>
2024-07-10 22:39:37 +0300

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,15 +9,16 @@
  */
 int _pow_recursion(int x, int y)
 {
-	int res = x;
-
 	if (y < 0)
+	{
 		return (-1);
-
-	else if (y == 0)
+	}
+	if (y == 0)
+	{
 		return (1);
-
-	res *= _pow_recursion(x, y - 1);
-
-	return (res);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
 }
