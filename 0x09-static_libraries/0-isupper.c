f9ba2131dc1b07d2e62fe8f7970431e6df13c564
lensonlovescode <lensonisinparis@gmail.com>
2024-07-12 21:31:30 +0300

    _GLOBAL_OFFSET_TABLE_

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * _isupper - checks is a character is uppercase
  * @c: carrier integer variable
  * Return: 1 if true, 0 if false
  */
+
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _memset - Fills the first n bytes of the memory area
  *		pointed to by @s with the constant byte @c
@@ -8,6 +9,7 @@
  *
  * Return: A pointer to the filled memory area @s.
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
