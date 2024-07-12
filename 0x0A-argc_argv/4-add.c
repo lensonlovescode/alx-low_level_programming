5c893eda5c08e909ba409ceca24803fce3c9b074
lensonlovescode <lensonisinparis@gmail.com>
2024-07-12 19:21:57 +0300

    initialize

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,11 +10,21 @@ int main(int argc, char *argv[])
 {
 	int sum;
 	int i;
-
+	int j;
+	
+	sum = 0;
 	i = 1;
 
 	while (i < argc)
 	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 		sum += atoi(argv[i]);
 		i++;
 	}
