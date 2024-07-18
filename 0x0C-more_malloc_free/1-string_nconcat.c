667385f07646a35a33cfe8f4573312dfb503868e
lensonlovescode <lensonisinparis@gmail.com>
2024-07-18 15:06:58 +0300

    cerrect version 100

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,39 +4,33 @@
  * string_nconcat - Concatenates two strings.
  * @s1: The string to be concatenated upon.
  * @s2: The string to be concatenated to s1.
- * @n: the number of bytes to be concatenated
- * Return: returns NULL on failure or pointer the new string
+ * @n: The number of bytes to be concatenated
+ * Return: returns NULL on failure or pointer to the new string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *big_str;
-	int len;
-	unsigned long int i, j;
+	unsigned int len1 = 0, len2 = 0, i, j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	i = 0;
-	len = 0;
-	while (s2[i] != '\0')
-		len++;
-		i++;
-	big_str = (char *)malloc((n * sizeof(char)) + 1);
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+
+	if (n >= len2)
+		n = len2;
+	big_str = malloc(sizeof(char) * (len1 + n + 1));
 	if (big_str == NULL)
 		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < len1; i++)
 		big_str[i] = s1[i];
-	j = 0;
-	if (n < len)
-		while (s2[j] != '\0' && j < n)
-			big_str[i + j] = s2[j];
-			j++;
-	else
-		while (s2[j] != '\0')
-			big_str[i + j] = s2[j];
-			j++;
-	big_str[i + j] = '\0';
+	for (j = 0; j < n; j++)
+		big_str[len1 + j] = s2[j];
+	big_str[len1 + j] = '\0';
 	return (big_str);
 }
 
