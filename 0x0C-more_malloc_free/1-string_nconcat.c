d432cc49ec7836a559df0d2ef4deb8e6dd3501dc
lensonlovescode <lensonisinparis@gmail.com>
2024-07-18 14:48:30 +0300

    check 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		while (s2[j] != '\0' && j < n)
 			big_str[i + j] = s2[j];
 			j++;
-	else if (n >= len)
+	else
 		while (s2[j] != '\0')
 			big_str[i + j] = s2[j];
 			j++;
