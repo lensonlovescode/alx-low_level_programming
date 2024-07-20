d70738ba9f8d31b750572033ca40588d2fa75abc
lensonlovescode <lensonisinparis@gmail.com>
2024-07-20 18:13:32 +0300

    free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>
 #include "dog.h"
-
 /**
  * free_dog - free memory allocated to the dog
  * @d: pointer to the dog
  * Return: void
-  */
+ */
 void free_dog(dog_t *d)
 {
 	if (d != NULL)
