b681eab8d2399a74579845affa8627265a9354ec
lensonlovescode <lensonisinparis@gmail.com>
2024-07-26 01:39:02 +0300

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,55 +13,38 @@ void print_all(const char * const format, ...)
 
 	j = 0;
 	start = 0;
-
 	va_start(list, format);
-
 	while (format && format[j] != '\0')
 	{
 		switch (format[j])
-		{
-			case 'c':
-				switch (start)
-				{
-					case 1:
-						printf(", ");
-				}
-				start = 1;
-				printf("%c", va_arg(list, int));
-				break;
+		{ case 'c':
+			switch (start)
+			{ case 1: printf(", "); }
+			start = 1;
+			printf("%c", va_arg(list, int));
+			break;
 			case 'i':
-				switch (start)
-				{
-					case 1: printf(", ");
-				}
-				start = 1;
-				printf("%i", va_arg(list, int));
-				break;
-			case 'f':
-				switch (start)
-				{
-					case 1: printf(", ");
-				}
-				start = 1;
-				printf("%f", va_arg(list, double));
-				break;
-			case's':
-				switch (start)
-				{
-					case 1: printf(", ");
-				}
-				start = 1;
+			switch (start)
+			{ case 1: printf(", "); }
+			start = 1;
+			printf("%i", va_arg(list, int));
+			break;
+		case 'f':
+			switch (start)
+			{ case 1: printf(", "); }
+			start = 1;
+			printf("%f", va_arg(list, double));
+			break;
+		case's':
+			switch (start)
+			{ case 1: printf(", "); }
+			start = 1;
 			ptr = va_arg(list, char*);
 			if (ptr)
-			{
-				printf("%s", ptr);
-				break;
-			}
+			{ printf("%s", ptr);
+			break; }
 			printf("%p", ptr);
-				break;
-		}
-		j++;
-	}
+			break; }
+		j++; }
 	printf("\n");
-	va_end(list);
-}
+	va_end(list); }
