2c7cefb71f403f7e8e74865558a468ee2ff8113b
lensonlovescode <lensonisinparis@gmail.com>
2024-08-01 20:58:55 +0300

    corrections

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -18,10 +18,6 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		new_node = malloc(sizeof(list_t));
 		new_node->str = strdup(str);
-		if (new_node->str == NULL)
-		{
-			return (NULL);
-		}
 		new_node->len = strlen(str);
 		new_node->next = NULL;
 		(*head) = new_node;
@@ -35,11 +31,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
-	new_node->next = NULL;
-	if (new_node->str == NULL)
-	{
-		return (NULL);
-	}
+	new_node->next =	}
 	while (ptr->next != NULL)
 	{
 		ptr = ptr->next;
