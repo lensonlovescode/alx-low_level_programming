2f540601dbed9a4f19a0f825652012b16e1a9a42
lensonlovescode <lensonisinparis@gmail.com>
2024-08-06 21:04:45 +0300

    end node!

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,11 +1,9 @@
 #include "lists.h"
 /**
- * delete_nodeint_at_index - Deletes the node at a given index in a listint_t linked list.
+ * delete_nodeint_at_index - Deletes the node at a given index
  * @head: A pointer to the pointer of the head of the listint_t linked list.
  * @index: The index of the node to be deleted, starting from 0.
- *
- * Return: If the node at the specified index is successfully deleted, returns 1.
- *         If it is not possible to delete the node at the specified index, returns -1.
+ * Return: returns 1 on succes, -1 on failure
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,29 +7,33 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_head;
+	listint_t *new_node, *temp;
 
-	new_head = malloc(sizeof(listint_t));
+	new_node = malloc(sizeof(listint_t));
 
-	if (new_head == NULL)
+	new_node->n = n;
+	new_node->next = NULL;
+
+	temp = (*head);
+
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
 
 	if ((*head) == NULL)
 	{
-		(*head) = new_head;
-		return (new_head);
+		(*head) = new_node;
+		return (new_node);
 	}
-
-	if (new_head == NULL)
+	else
 	{
-		return (NULL);
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new_node;
 	}
 
-	new_head->next = (*head);
-	new_head->n = n;
-	(*head) = new_head;
-
-	return (new_head);
+	return (new_node);
 }
