d5d246fb7a67246bad260e3b8f273f21c31f54b3
lensonlovescode <lensonisinparis@gmail.com>
2024-09-20 20:13:21 +0300

    sum

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * sum_dlistint - gets sum of data in all nodes of a linked list
+ * @head: the first node
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+        int sum;
+        dlistint_t *temp;
+
+        if (head == NULL)
+        {
+                return (0);
+        }
+
+        temp = head;
+        sum = 0;
+
+        while (temp != NULL)
+        {
+		sum += temp->n;
+                temp = temp->next;
+        }
+        return (sum);
+}
