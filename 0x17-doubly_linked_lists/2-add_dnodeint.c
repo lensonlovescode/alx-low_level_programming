c91c8c6f448cf634e3180c04c1285f90ed94a342
lensonlovescode <lensonisinparis@gmail.com>
2024-09-20 18:17:25 +0300

    add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 
-	new_head = (dlistint_t*)malloc(sizeof(dlistint_t));
+	new_head = (dlistint_t *)malloc(sizeof(dlistint_t));
 
 	if (new_head == NULL)
 	{
