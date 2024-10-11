6dd76ab2c873da5dc31341ac7a6713f1ad4914cd
lensonlovescode <lensonisinparis@gmail.com>
2024-10-11 20:48:46 +0300

    get value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_table.h"
+/**
+ * hash_table_get - gets the value associated with a key
+ * @ht: A pointer to the hash table.
+ * @key: The key to look for.
+ * Return: A pointer to the value, or NULL if the key
+ *         could not be found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL)
+	{
+		return (NULL);
+	}
+
+	index = key_index(key, ht->size);
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			return (node->value);
+		}
+		node = node->next;
+	}
+	return (NULL);
+}
