eaaf4f06c14f3265b5c8e8d1b784c79474d7c24d
lensonlovescode <lensonisinparis@gmail.com>
2024-10-11 22:45:37 +0300

    Final touches

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,8 +1,6 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_print - Prints the contents of a hash table.
- *
  * @ht: A pointer to the hash table.
  */
 void hash_table_print(const hash_table_t *ht)
